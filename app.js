4464289040349cc9b360292d25de001dd27b9841
Matias Godoy <matigodoy93@hotmail.com>
2021-02-01 17:30:40 -0300

    Partial Header

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const methodOverride = require('method-override')
 
 let app = express();
 
@@ -10,11 +11,16 @@ app.listen(port, () => {
 })
 
 // REQUIRE ROUTES
-const indexRouter = require('./routes/index');
+const indexRouter = require('./routes/indexRouter');
 
 // VIEW ENGINE SETUP
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 // MIDDLEWARES
 
